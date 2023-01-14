d39440d385907276e296f3c4d0f6f3478417ee10
Hyun Su <tlsgustn93@naver.com>
2023-01-14 23:54:01 +0900

    graph

---
+++ hyunsu/Graph/7579.py
@@ -0,0 +1,47 @@
+# 백준 7569 토마토 GOLD V
+# https://www.acmicpc.net/problem/7569
+
+from collections import deque
+
+m, n, h = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+q = deque()
+
+def bfs():
+    while q:
+        z, x, y = q.popleft()
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+
+            if 0 <= nx < n and 0 <= ny < m and 0 <= nz < h:
+                if graph[nz][nx][ny] == 0:
+                    graph[nz][nx][ny] = graph[z][x][y] + 1
+                    q.append((nz, nx, ny))
+
+
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if graph[i][j][k] == 1:
+                q.append((i, j, k))
+
+bfs()
+max_result = 0
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if graph[i][j][k] == 0:
+                print(-1)
+                exit(0)
+            else:
+                max_result = max(max_result, graph[i][j][k])
+
+print(max_result - 1)
